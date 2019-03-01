173553eb0117758fcf655828ff16109a43f08cbf
gabriel-desharnais <gabriel.desharnais@hotmail.com>
2019-03-01 00:35:19 -0500

    solving bad asgi stuff

--- GeoGIF/FrontEnd/FrontEnd/asgi.py
+++ GeoGIF/FrontEnd/FrontEnd/asgi.py
@@ -1,5 +1,12 @@
-import os
-import channels.asgi
+#import os
+#import channels.asgi
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chanelSimpleDemo.settings')
-channel_layer = channels.asgi.get_channel_layer()
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chanelSimpleDemo.settings')
+#channel_layer = channels.asgi.get_channel_layer()
+import os
+import django
+from channels.routing import get_default_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FrontEnd.settings")
+django.setup()
+application = get_default_application()
